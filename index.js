72aecc6518805da8f96f0b00791f00f2b71bd420
Camila Di Chiano <dichianocamila3@gmail.com>
2022-08-26 23:38:40 -0300

    ENTREGA

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+const edad = parseInt(prompt("Ingrese su edad para obtener acceso"));
+if(edad >= 15){
+    alert("Puedes ingresar al sitio");
+}else{
+    alert("No puedes ingresar al sitio");
+}
+
+
+
