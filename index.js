052a57c63282b571c394f69f2bfdeabdd2022399
Camila Di Chiano <dichianocamila3@gmail.com>
2022-08-30 23:23:46 -0300

    ENTREGA2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+// ENTREGA 1
+
 let numero = prompt("Ingrese su edad para obtener acceso");
 
 while( numero > 15);{
@@ -8,3 +10,38 @@ while( numero > 15);{
 
 alert("Bienvenid@");
 
+
+
+
+
+
+// ENTREGA 2
+
+
+// Promedio
+
+function promedio(nota1, nota2, nota3){
+    console.log((nota1 + nota2 + nota3)/3);
+}
+
+promedio(6,8,8);
+
+// ---------------------------------------------
+
+
+// Calcular costo total de productos y/o servicios seleccionados por el usuario.
+
+
+var miTotal = 0
+var monitor = 64000;
+var cpu = 51000;
+var teclado = 7000;
+var mouse = 1500;
+var suma = miTotal + monitor + cpu + teclado + mouse;
+
+function costoTotal(seleccionados){
+   this.seleccionados = seleccionados
+   console.log(suma);
+}
+
+costoTotal();
