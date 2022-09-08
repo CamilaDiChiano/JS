724941abcf26ad1b872061f23ffa3e20df7f2a78
Camila Di Chiano <dichianocamila3@gmail.com>
2022-09-07 22:57:06 -0300

    array

--- index.js
+++ index.js
@@ -1,47 +1,75 @@
 
-// ENTREGA 1
+// // ENTREGA 1
 
-let numero = prompt("Ingrese su edad para obtener acceso");
+// let numero = prompt("Ingrese su edad para obtener acceso");
 
-while( numero > 15);{
-    alert("No puedes ingresar al sitio");
-    numero  = prompt("Ingrese su edad para obtener acceso");
-}
+// while( numero > 15);{
+//     alert("No puedes ingresar al sitio");
+//     numero  = prompt("Ingrese su edad para obtener acceso");
+// }
 
-alert("Bienvenid@");
-
-
+// alert("Bienvenid@");
 
 
 
 
-// ENTREGA 2
+
+// // ENTREGA 2
 
 
-// Promedio
+// // Promedio
 
-function promedio(nota1, nota2, nota3){
-    console.log((nota1 + nota2 + nota3)/3);
-}
+// function promedio(nota1, nota2, nota3){
+//     console.log((nota1 + nota2 + nota3)/3);
+// }
 
-promedio(6,8,8);
+// promedio(6,8,8);
 
-// ---------------------------------------------
-
-
-// Calcular costo total de productos y/o servicios seleccionados por el usuario.
+// // ---------------------------------------------
 
 
-var miTotal = 0
-var monitor = 64000;
-var cpu = 51000;
-var teclado = 7000;
-var mouse = 1500;
-var suma = miTotal + monitor + cpu + teclado + mouse;
+// // Calcular costo total de productos y/o servicios seleccionados por el usuario.
 
-function costoTotal(seleccionados){
-   this.seleccionados = seleccionados
-   console.log(suma);
-}
 
-costoTotal();
+// var miTotal = 0
+// var monitor = 64000;
+// var cpu = 51000;
+// var teclado = 7000;
+// var mouse = 1500;
+// var suma = miTotal + monitor + cpu + teclado + mouse;
+
+// function costoTotal(seleccionados){
+//    this.seleccionados = seleccionados
+//    console.log(suma);
+// }
+
+// costoTotal();
+
+
+// ---------------------------------------------------------------------------
+
+// ARRAY
+
+
+const piedras = [
+    { id: 1, nombre: "Cuarzo azul", precio: 800 },
+    { id: 2, nombre: "Agata azul", precio: 880 },
+    { id: 3, nombre: "Citrino", precio: 790 },
+    { id: 4, nombre: "Turmalina negra", precio: 900 },
+  ];
+
+  for(const item of piedras){
+    console.log(item.nombre);
+    console.log(item.precio);
+  }
+
+  
+   
+
+
+
+
+
+
+
+
