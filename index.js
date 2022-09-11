b41191cdeae6ad0ecb106f49f8fdc18688a7ac04
Camila Di Chiano <dichianocamila3@gmail.com>
2022-09-11 00:24:51 -0300

    Primera entrega

--- index.js
+++ index.js
@@ -84,24 +84,115 @@ const piedras = [
 ];
 
 console.log(nombrePiedra);
+
+
 function mostrarPiedra(piedras){
 let piedraDeSuPreferencia = piedras.find((item) => nombrePiedra === item.nombre);
 
-alert( `La piedra de su preferencia es  ${piedraDeSuPreferencia} y cuesta ${piedraDeSuPreferencia.precio}`
+alert( `La piedra de su preferencia es  ${piedraDeSuPreferencia.nombre} y cuesta ${piedraDeSuPreferencia.precio}`
 );
 }
 
 mostrarPiedra(piedras);
 
-//   EJEMPLO: la persona elige las piedras que quiere comprar
+//   EJEMPLO: la persona elige dos piedras que quiere comprar
 
-let compraDePiedras = prompt("Ingrese el nombre de las piedras a comprar");
+let compraDePiedras1 = prompt("Ingrese el nombre de la piedra a comprar");
+console.log(compraDePiedras1);
+let compraDePiedras2 = prompt("Ingrese el nombre de la piedra a comprar");
+console.log(compraDePiedras2);
 
- var miTotal = 0
- var suma = miTotal + piedras.find((item) => compraDePiedras === item.nombre);
+const precioPiedra1 = piedras.find(piedras => piedras.nombre === compraDePiedras1);
+console.log(precioPiedra1.precio);
+const precioPiedra2 = piedras.find(piedras => piedras.nombre === compraDePiedras2);
+console.log(precioPiedra2.precio);
+var miTotal = 0
+var resultado= miTotal + precioPiedra1 + precioPiedra2
 
- function costoTotal(seleccionados) {
-     this.seleccionados = seleccionados
-     alert (`El costo total es ${suma}`);
- }
- costoTotal();
+
+function costoTotal(piedras) {
+    let todasLasPiedras = piedras.find((item) => compraDePiedras1 === item.precio) + piedras.find((item) => compraDePiedras2 === item.precio);
+    alert (`El costo total de los productos: ${compraDePiedras1} y ${compraDePiedras2} es: ${resultado}`);
+}
+
+costoTotal(piedras);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sumar (miTotal, compraDePiedras1, compraDePiedras2){
+//    let costo = miTotal + compraDePiedras1 + compraDePiedras2;
+//    alert (`El costo total de los productos: ${compraDePiedras1} y ${compraDePiedras2} es: ${costo}`);
+//     console.log(sumar);
+//  }
+
+//  sumar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function total(piedras){
+//     let compra = miTotal + piedras.find(item.precio);
+//     alert (`El costo total de los productos: ${compraDePiedras1} y ${compraDePiedras2} es: ${compra}`);
+// }
+
+// total();
+
+
+
+// function sumar (miTotal, compraDePiedras1, compraDePiedras2){
+//     let costo = miTotal + compraDePiedras1 + compraDePiedras2;
+//     alert (`El costo total de los productos: ${compraDePiedras1} y ${compraDePiedras2} es: ${costo}`);
+//     console.log(sumar);
+// }
+
+// sumar();
+
+
+// function compraTotal(compra){
+//     
+//     
+//     }
+
+//  compraTotal();
