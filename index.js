4f4173bb75e08d288296e5d6ab90b60e4be53868
Camila Di Chiano <dichianocamila3@gmail.com>
2022-09-30 23:10:36 -0300

    act

--- index.js
+++ index.js
@@ -1,70 +1,58 @@
 
 
-// let nosotros = document.getElementById ("nosotros");
-// let consulta = document.getElementById("consulta");
-
-// consulta.addEventListener("keyup" , (e) => {
-//     if(e.key === "enter"){
-//         nosotros.className = "rojo";
-//     }
-// });
-
-// console.log(consulta);
-
-
-// --------------------------------
-
-const productosTotales = [
-    {id: 1, nombre: "Amatista", prrecio: 760},
-    {id: 2, nombre: "Citrino", precio: 890},
-    {id: 3, nombre: "Agata azul", precio: 850},
-    {id: 4, nombre: "Cuarzo de cristal", precio: 800},
-    {id: 5, nombre: "Cuarzo rosa", precio: 960},
-    {id: 6, nombre: "Jaspe rojo", precio: 890},
-    {id: 7, nombre: "Onix negro", precio: 950},
-    {id: 8, nombre: "Turmalina negra", precio: 900},
-    {id: 9, nombre: "Anillo Amatista", prrecio: 1160},
-    {id: 10, nombre: "Anillo de Citrino", precio: 1250},
-    {id: 11, nombre: "Anillo de Agata azul", precio: 1050},
-    {id: 12, nombre: "Anillo de Cuarzo de cristal", precio: 1200},
-    {id: 13, nombre: "Anillo de Cuarzo rosa", precio: 1260},
-    {id: 14, nombre: "Anillo de Jaspe rojo", precio: 1300},
-    {id: 15, nombre: "Anillo de Onix negro", precio: 1350},
-    {id: 16, nombre: "Anillo de Turmalina negra", precio: 1100},
-];
-
-
 localStorage.setItem("productos", JSON.stringify(productosTotales));
 
-let carrito = [];
+
 let productosStorage = JSON.parse(localStorage.getItem("carrito"));
 
-if(productosStorage){
+if (productosStorage) {
     carrito = productosStorage;
 }
 
 
-productosTotales.forEach(producto =>{
-    let div = document.createElement("div");
-div.innerHTML =`<h1>Carrito: ${producto.id}</h1>
-<p>Nombre: ${producto.nombre}</p>
+const contenedorProductos = document.getElementById('contenedorProductos');
+
+const contenedorDelCarrito = document.getElementById('carritoContenedor')
+
+let carrito = [];
+
+productosTotales.forEach(producto => {
+    const div = document.createElement("div");
+    div.innerHTML = `<img src= ${producto.img} class = "img">
+<h3>${producto.nombre}</h3>
 <p>$ ${producto.precio}</p>
+
 <button id = "agregar ${producto.id}" class = "boton-agregar"</button>
 `;
 
- document.body.append(div);
+    contenedorProductos.appendChild(div)
 
-const boton = document.getElementById("agregar ${producto.id}")
+    const boton = document.getElementById("agregar ${producto.id}")
 
-button.addEventListener("click", () => {
-    agregarAlCarrito(productosTotales.id)
+    // boton.addEventListener('click', () => {
+    //     agregarAlCarrito(producto.id)
+    // })
+
 })
 
-});
 
 
 const agregarAlCarrito = (productosId) => {
     const item = productosTotales.find((prod) => prod.id === productosId)
     carrito.push(item)
     console.log(carrito);
+}
+
+const actualizacionCarrito = () => {
+
+carrito.forEach((prod) => {
+    const div= document.getElementById('div')
+div.innerHTML=`
+<p>${prod.nombre}</p>
+<p>precio${prod.precio}</p>
+
+`
+
+})
+
 }
---
+++ productos.js
@@ -0,0 +1,20 @@
+let productosTotales = [
+    {id: 1, nombre: "Amatista", precio: 760, img: '../img/amatista.jpg'},
+    {id: 2, nombre: "Citrino", precio: 890, img: '../img/citrino.jpg'},
+    {id: 3, nombre: "Agata azul", precio: 850, img: '../img/agata azul.jpg'},
+    {id: 4, nombre: "Cuarzo de cristal", precio: 800, img: '../img/cuarzo cristal.jpg'},
+    {id: 5, nombre: "Cuarzo rosa", precio: 960, img: '../img/cuarzo rosa.jpg'},
+    {id: 6, nombre: "Jaspe rojo", precio: 890, img: '../img/jaspe rojo.jpg'},
+    {id: 7, nombre: "Onix negro", precio: 950, img: '../img/onix negro.jpg'},
+    {id: 8, nombre: "Turmalina negra", precio: 900, img: '../img/turmalina negra.jpg'},
+    {id: 9, nombre: "Anillo Amatista", prrecio: 1160, img: '../img/anilloamatista.jpg'},
+    {id: 10, nombre: "Anillo de Citrino", precio: 1250, img: '../img/anillocitrino.jpg'},
+    {id: 11, nombre: "Anillo de Agata azul", precio: 1050, img: '../img/anilloagataazul.jpg'},
+    {id: 12, nombre: "Anillo de Cuarzo de cristal", precio: 1200, img: '../img/anillocuarzodecristal'},
+    {id: 13, nombre: "Anillo de Cuarzo rosa", precio: 1260, img: '../img/anillocuarzorosa.webp.crdownload'},
+    {id: 14, nombre: "Anillo de Jaspe rojo", precio: 1300, img: '../img/anillojasperojo.jpg'},
+    {id: 15, nombre: "Anillo de Onix negro", precio: 1350, img: '../img/anilloonixnegro.webp'},
+    {id: 16, nombre: "Anillo de Turmalina negra", precio: 1100, img: '../img/anilloturmalinanegra.jpg'},
+];
+
+console.log(productosTotales);
